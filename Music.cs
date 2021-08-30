97ee1f6bfe74d93e42d0ac4902c104c9460565b9
Joubert <joubertvanzyl10@gmail.com>
2021-08-30 15:30:44 +0200

    Added Player.cs and Music.cs classes
    Music class has the Genre enum, it is public so every class should have access to it

---
+++ Music.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_PRG
+{
+    public enum Genre
+    {
+        Rock,
+        Pop,
+        Jazz,
+        Country,
+        Reggae,
+        HeavyMetal,
+        Gospel,
+        Dubstep
+    }
+
+    class Music
+    {
+        private string Name;
+        private Genre Genre;
+
+        public Music(string Name, Genre Genre)
+        {
+            this.Name = Name;
+            this.Genre = Genre;
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_PRG
+{
+    class Player
+    {
+        private string Name;
+        private string Password;
+        private int Score;
+
+        public Player(string Name, string Password)
+        {
+            this.Name = Name;
+            this.Password = Password;
+        }
+
+        // Add score to the player by a custom amount. Default is 1
+        public void AddScore(int Amount = 1) { Score += Amount; }
+
+        // Reset player score back to zero
+        public void ResetScore() { Score = 0; }
+    }
+}
