7a60418e3c5adfa51da198706bccf74e2b85e266
Joubert <joubertvanzyl10@gmail.com>
2021-08-31 12:48:55 +0200

    Implemented our custom timer using delegates
    e.g. of timer being used:
    
    static void Tick(int TimeRemaining) { }
    
    MusicTimer timer = new MusicTimer(15);
    timer.Start(Tick);

---
+++ MusicTimer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Project_PRG
+{
+    // This class requires "using System.Threading;" at the top of the class to work
+    public class MusicTimer
+    {
+        private int Time = 0;
+
+        public MusicTimer(int TimeToRunForInSeconds = 10)
+        {
+            Time = TimeToRunForInSeconds;
+        }
+
+        public void Start(Action<int> TickFunction)
+        {
+            // The function passed in needs to be marked "static"
+            TickFunction(Time);
+            Time -= 1;
+
+            Thread.Sleep(1000);
+
+            if (Time > 0)
+                Start(TickFunction);
+        }
+    }
+}
